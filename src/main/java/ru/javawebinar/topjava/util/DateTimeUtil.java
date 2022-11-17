4d2fc1aa267f5a008f1af0ac87800175735bae2f
sosedvovan <drugst@yandex.ru>
2022-11-17 12:13:27 +0300

    HW1_CRUD on servlets

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -3,7 +3,7 @@ package ru.javawebinar.topjava.util;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
+//
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
