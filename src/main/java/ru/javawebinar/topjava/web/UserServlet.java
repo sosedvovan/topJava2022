0cbebf44fae7782147bb2109226d1b9de8bca810
sosedvovan <drugst@yandex.ru>
2022-11-17 09:46:37 +0300

    lesson01 tomcat+servlets

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -11,6 +11,9 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+        response.sendRedirect("users.jsp");// "/" перед users.jsp не ставь чтобы урл был
+        //от контекста (http://localhost:8080/topjava/users.jsp)
+        //а если поставить "/users.jsp" тобудет от рута (http://localhost:8080/users.jsp)
+        //и не найдет страницу тк у нас контекст /topjava задан в run-> Edit Configuration
     }
 }
